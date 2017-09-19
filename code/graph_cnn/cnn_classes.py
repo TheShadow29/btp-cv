89d03371e375a79791919e22a6a3b8ac48465e73
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-09-19 12:43:12 +0530

    Partial progress to cnn_classes

--- code/graph_cnn/cnn_classes.py
+++ code/graph_cnn/cnn_classes.py
@@ -2,28 +2,39 @@
 import numpy as np
 # import sklearn
 import networkx as nx
-
+import pdb
 
 class gcn_layer(object):
-    def __init__(self, adj_graph, is_sparse=False):
+    # def __init__(self, adj_graph, is_sparse=False):
+    def __init__(self, _G):
         # for the time being assuming that
         # one graph is enough to represent one layer
-        if is_sparse:
-            self.G = nx.from_scipy_sparse_matrix(adj_graph)
-        else:
-            self.G = nx.from_numpy_matrix(adj_graph)
-        self.sparse_adj_mat = self.G.adjacency_matrix()
+        # if is_sparse:
+        # self.G = nx.from_scipy_sparse_matrix(adj_graph)
+        # else:
+        # self.G = nx.from_numpy_matrix(adj_graph)
+        self.G = _G
+        # self.sparse_adj_mat = self.G.adjacency_matrix()
         self.coarse_graphs = list()
+        self.coarse_graphs.append(self.G)
 
-    # def metis(self, levels):
-    #     # Coarsen a graph multiple times using the Metis algorithm
-    #     # levels is the number of coarsened graphs
-    #     self.coarse_graphs.append(G)
-    #     # rid = np
-    #     for i in range(levels):
-    # Need to choose the weights for pairing
+    def one_level(self, graph_ind):
+        # G is a networkx graph
+        G = self.coarse_graphs[graph_ind]
+        adj_mat = nx.adjacency_matrix(G)
+        adj_mat_coo = adj_mat.tocoo()
+        degree_vec = adj_mat_coo.sum(axis=0)
+        rr, cc, vv = adj_mat_coo.row, adj_mat_coo.col, adj_mat_coo.data
+        pdb.set_trace()
+        marked_ind = np.zeros(rr.shape[0])
+        for i in range(rr.shape[0]):
+            if not marked_ind[i]:
+                marked_ind[i] = True
+                grac_max = -np.inf
+                # for j in
 
-    # def coarsen(self):
-    # Coarsen a graph
 
-    # def metis_one_level(self, ):
+if __name__ == '__main__':
+    grid_graph = nx.grid_graph(dim=[28, 28])
+    glayer = gcn_layer(grid_graph)
+    glayer.one_level(0)
