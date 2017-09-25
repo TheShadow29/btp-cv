16252f44880d77846bedd784c4e55e1bcbcdc2fd
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-09-25 20:28:19 +0530

    Vanilla cnn code written, data loader and testing remains

--- code/cnn_ecg/ecg_cnn.py
+++ code/cnn_ecg/ecg_cnn.py
@@ -3,6 +3,7 @@ import pdb
 import time
 # import pickle
 import torch
+from torch.autograd import Variable
 
 
 class ecg_one_data_holder():
@@ -44,3 +45,36 @@ if __name__ == '__main__':
     #     pickle.dump(ecg_all_data, f)
     # pickling is a bad idea goes more than 4 gb
     # Instead populate only relevant data
+
+    # Pytorch code from here
+    # Use 50% control and 50% positive people for training
+    # That should ideally remove any training bias (hopefully)
+    D_in = 3000
+    D_out = 2
+    H = 300
+
+    inp_data = Variable(torch.zeros(3000))
+    # out_data = Variable(torch.zeros(2))
+
+    simple_model = torch.nn.Sequential(
+        torch.nn.Linear(D_in, H),
+        torch.nn.ReLU(),
+        torch.nn.Linear(H, D_out),
+        torch.nn.LogSoftmax(),
+    )
+
+    # loss_fn = torch.nn.CrossEntropyLoss()
+    loss_fn = torch.nn.NLLLoss()
+    optimizer = torch.optim.Adam(simple_model.parameters())
+
+    # Need to write script for getting in the data
+    # data_loader =
+
+    for epoch in range(30):
+        for instance, label in data_loader:
+            y_pred = simple_model(instance)
+            loss = loss_fn(y_pred, label)
+            print(epoch, loss.data[0])
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
