2fd95851cd158223eb46a0fdaec5d04ab1ef8bd3
Arka Sadhu <ark.sadhu2904@gmail.com>
2018-01-06 18:28:35 +0530

    Still some errors left

--- code/cnn_ecg/ecg_cnn_graph.py
+++ code/cnn_ecg/ecg_cnn_graph.py
@@ -7,6 +7,12 @@ import collections
 import time
 import numpy as np
 from torchvision import datasets, transforms
+from lib.grid_graph import grid_graph
+from lib.coarsening import coarsen
+from lib.coarsening import lmax_L
+from lib.coarsening import perm_data
+from lib.coarsening import rescale_L
+
 
 import sys
 sys.path.insert(0, 'lib/')
@@ -47,3 +53,33 @@ if __name__ == '__main__':
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=test_batch_size, shuffle=True, **kwargs)
+
+    # Construct the Graph
+
+    t_start = time.time()
+    grid_side = 28
+    number_edges = 8
+    metric = 'euclidean'
+    A = grid_graph(grid_side, number_edges, metric)  # create graph of Euclidean grid
+
+    # Compute coarsened graphs
+    coarsening_levels = 4
+    L, perm = coarsen(A, coarsening_levels)
+
+    # Compute max eigenvalue of graph Laplacians
+    lmax = []
+    for i in range(coarsening_levels):
+        lmax.append(lmax_L(L[i]))
+    print('lmax: ' + str([lmax[i] for i in range(coarsening_levels)]))
+
+    # Reindex nodes to satisfy a binary tree structure
+    train_data = perm_data(train_data, perm)
+    val_data = perm_data(val_data, perm)
+    test_data = perm_data(test_data, perm)
+
+    print(train_data.shape)
+    print(val_data.shape)
+    print(test_data.shape)
+
+    print('Execution time: {:.2f}s'.format(time.time() - t_start))
+    del perm
